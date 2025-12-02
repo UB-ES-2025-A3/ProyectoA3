4b05e09a6d72b55da1f6ff68e6d7ec018d1087b3
anna haro <annaharo00@gmail.com>
2025-12-02 20:23:46 +0100

    name corrected

--- frontend/src/components/layout/NavBar.js
+++ frontend/src/components/layout/NavBar.js
@@ -15,7 +15,7 @@ export default function NavBar() {
 
   return (
     <nav className="nav">
-      <div className="nav__brand">Trips & Events</div>
+      <div className="nav__brand">EventManager</div>
       <div className="nav__links">
         <NavLink to="/events">Eventos</NavLink>
         <NavLink to="/profile">Perfil</NavLink>
